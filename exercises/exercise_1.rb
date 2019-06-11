728dd5634f08b5fc6c2d425c8a064371dd818041
Devanm15 <you@example.com>
2019-06-11 21:20:39 +0000

    exercise 2 loaded stores by id and updated store1.name

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -5,8 +5,8 @@ puts "----------"
 
 # Your code goes below here ...
 
-Store.create(id: 1, name: "Burnaby", annual_revenue: 300000, mens_apparel: 1, womens_apparel: 1 )
-Store.create(id: 2, name: 'Richmond', annual_revenue: 1260000, mens_apparel: 0, womens_apparel: 1) 
-Store.create(id: 3, name: 'Gastown', annual_revenue: 190000, mens_apparel: 1, womens_apparel: 0 )
+Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: 1, womens_apparel: 1 )
+Store.create(name: 'Richmond', annual_revenue: 1260000, mens_apparel: 0, womens_apparel: 1) 
+Store.create(name: 'Gastown', annual_revenue: 190000, mens_apparel: 1, womens_apparel: 0 )
 
 puts Store.all.count
