83b97bef03d1c18c67c7976cd8efba700510cb1d
Devanm15 <you@example.com>
2019-06-11 22:18:00 +0000

    Exercise 5 complete

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,10 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+@total_sum = Store.all.sum("annual_revenue")
+puts @total_sum
+
+puts @total_sum / Store.all.count
+
+puts Store.where("annual_revenue > 1000000" ).count
